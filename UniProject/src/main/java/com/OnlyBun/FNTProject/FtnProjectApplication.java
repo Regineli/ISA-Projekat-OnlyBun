6cdca2358e308bedcc88a81ec0ad2e4228cc58d8
Regineli <45895861+Regineli@users.noreply.github.com>
2024-10-26 16:34:27 +0200

    Maven Imported

---
+++ UniProject/src/main/java/com/OnlyBun/FNTProject/FtnProjectApplication.java
@@ -0,0 +1,14 @@
+package com.OnlyBun.FNTProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FtnProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FtnProjectApplication.class, args);
+		System.out.println("Test test");
+	}
+
+}
